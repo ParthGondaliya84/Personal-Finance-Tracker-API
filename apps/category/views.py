7cf791a4d5232b502efe462a81f99588b1eb33c4
Parth Gondaliya <parthpatel848884@gmail.com>
2025-04-23 15:54:27 +0530

    add filter

--- apps/category/views.py
+++ apps/category/views.py
@@ -2,11 +2,15 @@ from rest_framework import viewsets
 from apps.base.views import BaseViewSet
 from apps.category.models import Category
 from apps.category.serializers import CategorySerializer
+from apps.category.filters import CategoryFilters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CategoryAPIView(BaseViewSet, viewsets.ModelViewSet):
-    serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CategoryFilters
 
     def get_queryset(self):
         user = self.request.user
