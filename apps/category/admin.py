386cf6eb8000671a6a999a96741921b84fa1905d
Parth Gondaliya <parthpatel848884@gmail.com>
2025-04-23 15:59:31 +0530

    Revert "Merge pull request #6 from ParthGondaliya84/task/category"
    This reverts commit 37b8dc37263d5158aa3428f9c944ec443ce33bba, reversing
    changes made to 9e198f45d741ebb57e1a0bb43b295174a2e5ffc2.

--- apps/category/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from apps.category.models import Category
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["title", "entry_type", "id", "created_by"]
-
-
-admin.site.register(Category, CategoryAdmin)
--- apps/category/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from apps.category.views import CategoryAPIView
-
-router = DefaultRouter()
-router.register(r'category', CategoryAPIView, basename='category')
-
-urlpatterns = router.urls
